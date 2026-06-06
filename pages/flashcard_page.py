6f5507da9b3ccd375fcc6a6a5b54c7ec252ce93a
KimY <kimy2212004@gmail.com>
2026-06-06 23:17:22 +0700

    test: add flashcard tests

--- pages/flashcard_page.py
+++ pages/flashcard_page.py
@@ -5,15 +5,25 @@ from selenium.common import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
+from selenium.webdriver.common.keys import Keys
 
 class FlashcardPage(BasePage):
 
     FLASHCARD_CARDS = (By.XPATH, "//div[@class='col-6 col-md-3']")
     WORD_CARDS = (By.XPATH, "//div[@class='termlist-item contentblock']")
+    DEFINE_LINE_IN_WORD_CARD = (By.XPATH, "//div[@class='prewrap mb-2']")
+    WORD_TYPE_LINE = (By.CSS_SELECTOR, "body > div:nth-child(4) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(8) > div:nth-child(1) > div:nth-child(2) > h2:nth-child(1) > span:nth-child(1)")
+    PRONUNCIATION_LINE = (By.CSS_SELECTOR, "body > div:nth-child(4) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(8) > div:nth-child(1) > div:nth-child(2) > h2:nth-child(1) > span:nth-child(2)")
+    EXAMPLE_LINE = (By.XPATH, "//ul[@class='termlist-item-examples']")
+    NOTE_LINE = (By.XPATH, "//div[@class='prewrap']")
+    PICTURE_AREA = (By.XPATH, "//div[@class='termlist-item-images']//img[@class='lazyel entered loaded']")
+
+    CHOOSE_PICTURE = (By.XPATH, "//input[@id='id_image']")
     EDIT_BUTTON = (By.XPATH, "//a[@class='font-1 ml-1']")
     EDIT_FORM = (By.XPATH, "//div[@id='form-modal-content']")         
     DEFINE_INPUT = (By.ID, "field-definition")          
-    SHOW_OPTION = (By.ID, "headingOne")                 
+    SHOW_OPTION = (By.ID, "headingOne")  
+    NEWWORD_INPUT = (By.XPATH, "//input[@id='id_name']")               
     CLOSE_FORM_BUTTON = (By.XPATH, "//div[@id='form-modal']//button[@aria-label='Close']")
     SAVE_BUTTON = (By.XPATH, "//button[contains(text(),'Lưu')]")
     LABEL_INPUT = (By.XPATH, "//div[@id='form-modal-content']//label")
@@ -25,6 +35,16 @@ class FlashcardPage(BasePage):
     EASY_BUTTON = (By.XPATH, "//div[@class='flashcard-action action-easy text-success']")
     HARD_BUTTON = (By.XPATH, "//div[@class='flashcard-action action-difficult text-danger']")
     KNOWN_BUTTON = (By.XPATH, "//div[@class='flashcard-action action-ignore text-gray']")
+
+    REVIEW_CARD = (By.XPATH, "//div[@class='flashcard-review']")
+    DEFINE_IN_REVIEW_CARD = (By.XPATH, "//div[@class='prewrap mb-2']")
+    ANSWER_INPUT = (By.XPATH, "//input[@placeholder='Điền từ vào ô trống ...']")
+
+    OPTIONS_IN_REVIEW_CARD = (By.XPATH, "//div[@class='flashcard-review-option']")
+    KNOWN_IN_REVIEW_CARD = (By.XPATH, "//div[@class='float-right']")
+
+    SHOW_ANSWER_BUTTON = (By.XPATH, "//span[@class='link flashcard-review-showanswer']")
+    CONTINUE_BUTTON = (By.XPATH, "//span[@class='float-right link flashcard-review-skip']")
 
     def load(self):
         self.driver.get("https://study4.com/flashcards/")
@@ -50,8 +70,12 @@ class FlashcardPage(BasePage):
                 break
     
     def click_show_options(self):
-        return self.click(self.SHOW_OPTION)
-                
+
+        self.click(self.SHOW_OPTION)
+        return True
+    
+    def get_text_newword_input(self):
+        return self.find_element(self.NEWWORD_INPUT).get_attribute("value")
     
     def click_close_form_button(self):
         return self.click(self.CLOSE_FORM_BUTTON)
@@ -91,10 +115,6 @@ class FlashcardPage(BasePage):
         self.type_text(self.DEFINE_INPUT, text)
 
     def type_text_field_by_text_label(self, label_text, text):
-        import time
-        time.sleep(1) # Đợi modal render xong tránh StaleElement
-        if label_text not in "Định nghĩa":
-            self.click_show_options()
         labels = self.find_elements(self.LABEL_INPUT)
         for label in labels:
             if label_text in label.text.strip():
@@ -137,7 +157,55 @@ class FlashcardPage(BasePage):
 
                 break
 
+    def upload_image_to_image_field(self, image_path):
+        input_image = self.find_element(self.CHOOSE_PICTURE)
+        input_image.send_keys(image_path)
 
+    def get_text_of_define_in_wordcard(self, word):
+        words = self.find_elements(self.WORD_CARDS)
+        for w in words:
+            if word in w.text:
+                return w.find_element(*self.DEFINE_LINE_IN_WORD_CARD).text
+        return None
+
+    def get_word_type(self, word):
+        """Lấy toàn bộ text của word card chứa từ đã cho để kiểm tra định nghĩa"""
+        words = self.find_elements(self.WORD_CARDS)
+        for w in words:
+            if word in w.text:
+                return w.find_element(*self.WORD_TYPE_LINE).text
+        return None
+
+    def get_word_pronunciation(self, word):
+        """Lấy toàn bộ text của word card chứa từ đã cho để kiểm tra định nghĩa"""
+        words = self.find_elements(self.WORD_CARDS)
+        for w in words:
+            if word in w.text:
+                return w.find_element(*self.PRONUNCIATION_LINE).text
+        return None
+
+    def get_word_example(self, word):
+        """Lấy toàn bộ text của word card chứa từ đã cho để kiểm tra định nghĩa"""
+        words = self.find_elements(self.WORD_CARDS)
+        for w in words:
+            if word in w.text:
+                return w.find_element(*self.EXAMPLE_LINE).text
+        return None
+
+    def get_word_note(self, word):
+        """Lấy toàn bộ text của word card chứa từ đã cho để kiểm tra định nghĩa"""
+        words = self.find_elements(self.WORD_CARDS)
+        for w in words:
+            if word in w.text:
+                return w.find_element(*self.NOTE_LINE).text
+        return None
+
+    def get_word_picture(self, word):
+        words = self.find_elements(self.WORD_CARDS)
+        for w in words:
+            if word in w.text:
+                return w.find_element(*self.PICTURE_AREA).get_attribute("src")
+        return None
     def get_word_definition(self, word):
         """Lấy toàn bộ text của word card chứa từ đã cho để kiểm tra định nghĩa"""
         words = self.find_elements(self.WORD_CARDS)
@@ -145,6 +213,7 @@ class FlashcardPage(BasePage):
             if word in w.text:
                 return w.text
         return None
+
 
     def is_required_error_displayed(self):
         try:
@@ -206,15 +275,77 @@ class FlashcardPage(BasePage):
     
     def click_easy_button(self):
         self.switch_to_flashcard_iframe()
-        self.click(self.EASY_BUTTON)
+        element = self.find_element(self.EASY_BUTTON)
+        element.click()
 
     def click_hard_button(self):
         self.switch_to_flashcard_iframe()
-        self.click(self.HARD_BUTTON)
+        element = self.find_element(self.HARD_BUTTON)
+        element.click()
 
     def click_known_button(self):
         self.switch_to_flashcard_iframe()
-        self.click(self.KNOWN_BUTTON)
+        element = self.find_element(self.KNOWN_BUTTON)
+        element.click()
 
         
+    def check_visible_review_card(self):
+        self.switch_to_flashcard_iframe()
+        return self.is_visible(self.REVIEW_CARD)
 
+    def get_text_in_review_card(self):
+        self.switch_to_flashcard_iframe()
+        return self.get_text(self.REVIEW_CARD)
+    
+    def check_answer_input(self):
+        self.switch_to_flashcard_iframe()
+        return self.is_visible(self.ANSWER_INPUT)
+    
+    def type_answer_input(self, text):
+        self.switch_to_flashcard_iframe()
+        self.type_text(self.ANSWER_INPUT, text)
+
+    def clear_answer_input(self):
+        self.switch_to_flashcard_iframe()
+        self.clear_text(self.ANSWER_INPUT)
+
+    def press_enter(self):
+        self.switch_to_flashcard_iframe()
+        element = self.find_element(self.ANSWER_INPUT)
+        element.send_keys(Keys.ENTER)
+
+    def get_define_in_review_card(self):
+        self.switch_to_flashcard_iframe()
+        return self.get_text(self.DEFINE_IN_REVIEW_CARD)
+
+    def click_known_in_review_card(self):
+        self.switch_to_flashcard_iframe()
+        self.click(self.KNOWN_IN_REVIEW_CARD)
+
+    def click_show_answer(self):
+        self.switch_to_flashcard_iframe()
+        self.click(self.SHOW_ANSWER_BUTTON)
+
+    def get_text_in_answer_input(self):
+        self.switch_to_flashcard_iframe()
+        return self.find_element(self.ANSWER_INPUT).get_attribute("value")
+
+    def check_visiable_options_in_review_card(self):
+        self.switch_to_flashcard_iframe()
+        try:
+            elements = self.wait.until(EC.visibility_of_all_elements_located(self.OPTIONS_IN_REVIEW_CARD))
+            return len(elements) > 0
+        except TimeoutException:
+            return False
+    
+    def choose_options_in_review_card(self, answer):
+        self.switch_to_flashcard_iframe()
+        options = self.find_elements(self.OPTIONS_IN_REVIEW_CARD)
+        for option in options:
+            if answer.strip().lower() in option.text.strip().lower():
+                option.click()
+                break
+        
+    def click_continue_button(self):
+        self.switch_to_flashcard_iframe()
+        self.click(self.CONTINUE_BUTTON)
