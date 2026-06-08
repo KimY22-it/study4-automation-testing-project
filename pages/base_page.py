76d4bcd689a4c39378cff5bb2862ee4c4cbe57ca
KimY <kimy2212004@gmail.com>
2026-06-08 13:15:18 +0700

    fix: update wait time

--- pages/base_page.py
+++ pages/base_page.py
@@ -10,7 +10,7 @@ import time
 class BasePage:
     def __init__(self, driver):
         self.driver = driver
-        self.wait = WebDriverWait(driver, 10)
+        self.wait = WebDriverWait(driver, 2)
     
     def open_url(self, url):
         self.driver.get(url)
